3149b38c5c29de68b2dd929c1fee82667216a5c5
Your Name <you@example.com>
2020-03-09 07:59:19 -0400

    update run file

--- run.sh
+++ run.sh
@@ -4,11 +4,12 @@ python Main.py \
 --unsup_ratio 5 \
 --batch_size 8 \
 --unsup_batch_size 30 \
---data_root /media/nasir/Drive1/code/chexnet/database/xrays \
+--data_root database/xrays \
 --file_train new_split/train_sup.txt \
 --file_train_unsup new_split/train_unsup.txt \
 --file_val new_split/val_list.txt \
 --file_test new_split/test_list.txt \
---uda  \
+# --uda  \
 
 # --checkpoint checkpoints/m-09032020-104523_best_auroc.pth.tar
+# /media/nasir/Drive1/code/chexnet/
